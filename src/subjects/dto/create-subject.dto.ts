c461a4e5d7f703db88875de1ae76797d851afcc9
baraboulka <75359803+baraboulka@users.noreply.github.com>
2021-02-22 10:49:43 +0200

    Added class validator and added dto validation

--- src/subjects/dto/create-subject.dto.ts
+++ src/subjects/dto/create-subject.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
+
 export class CreateSubjectDto {
+    @IsString()
+    @IsNotEmpty()
     readonly title: string
+
+    @IsString()
+    @IsNotEmpty()
     readonly teacher: string
+
+    @IsNumber()
+    @IsNotEmpty()
     readonly weeklyAmount: number
 }
--- src/subjects/dto/update-subject.dto.ts
+++ src/subjects/dto/update-subject.dto.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
 export class UpdateSubjectDto {
+    @IsString()
+    @IsNotEmpty()
     readonly title: string
+
+    @IsString()
+    @IsNotEmpty()
     readonly teacher: string
+
+    @IsNumber()
+    @IsNotEmpty()
     readonly weeklyAmount: number
 }
